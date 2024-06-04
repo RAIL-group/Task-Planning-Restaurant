ee5d6ce552eb427dd819395a7924033f52bfcced
Ridwan <akkhar.092@gmail.com>
2024-06-03 20:51:21 -0400

    updated demo

--- modules/taskplan/taskplan/scripts/demo_pddl.py
+++ modules/taskplan/taskplan/scripts/demo_pddl.py
@@ -6,11 +6,13 @@ import taskplan
 def run_pddl():
     # preparing pddl as input to the solver
     pddl = {}
+    restaurant = taskplan.environments.restaurant.RESTAURANT(seed=1)
     pddl['domain'] = taskplan.pddl.domain.get_domain()
-    pddl['problem'] = taskplan.pddl.problem.get_problem()
+    pddl['problem'] = taskplan.pddl.problem.get_problem(restaurant)
     pddl['planner'] = 'max-astar'
 
     plan, cost = solve_from_pddl(pddl['domain'], pddl['problem'], planner=pddl['planner'])
+    print(plan)
 
     if plan:
         for p in plan:
