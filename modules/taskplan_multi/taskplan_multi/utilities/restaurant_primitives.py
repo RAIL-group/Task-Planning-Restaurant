660dafb0e15affc8f963cf386b60ca3f67c34d82
Ridwan <akkhar.092@gmail.com>
2025-06-12 14:57:25 -0400

    track: head

---
+++ modules/taskplan_multi/taskplan_multi/utilities/restaurant_primitives.py
@@ -0,0 +1,2 @@
+KITCHEN_CONTAINERS = ['countertop','shelf', 'cabinet', 'stove', 'fridge', 'pantry', 'sink']
+SERVING_ROOM_CONTAINERS = ['servingtable1', 'servingtable2', 'bussingcart', 'sanitizerstation']
